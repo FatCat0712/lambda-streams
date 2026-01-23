07971b529a50875f3491c6013b2eb7b90058a454
Fatcat0712 <maisonhai456@gmail.com>
2026-01-23 14:14:39 +0700

    easier exception handling for relfective methods

---
+++ src/eazybytes/ExceptionHandlingForReflectiveMethods.java
@@ -0,0 +1,35 @@
+package eazybytes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.NoSuchElementException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionHandlingForReflectiveMethods {
+    private final static Logger LOGGER = Logger.getLogger(ExceptionHandlingForReflectiveMethods.class.getName());
+
+    public static void main(String[] args) {
+        beforeJava7();
+        withJava7();
+    }
+
+
+    public static void beforeJava7() {
+        try {
+            Class.forName("eazybytes.CatchingMultipleExceptions").getMethod("withJava7").invoke(null, new Object[]{});
+        } catch (IllegalAccessException | InvocationTargetException | ClassNotFoundException |
+                 NoSuchMethodException ex) {
+            LOGGER.log(Level.SEVERE, ex.toString());
+        }
+    }
+
+    public static void withJava7() {
+        try {
+            Class.forName("eazybytes.CatchingMultipleExceptions").getMethod("withJava7").invoke(null, new Object[]{});
+        } catch (ReflectiveOperationException nex) {
+            LOGGER.log(Level.SEVERE, nex.toString());
+        }
+    }
+
+
+}
