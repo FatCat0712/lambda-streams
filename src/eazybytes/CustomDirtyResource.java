3f168b28f2e7a78ff1fb52decd66b426e3d47765
Fatcat0712 <maisonhai456@gmail.com>
2026-01-23 11:11:28 +0700

    Suppressed Exceptions

---
+++ src/eazybytes/CustomDirtyResource.java
@@ -0,0 +1,13 @@
+package eazybytes;
+
+public class CustomDirtyResource implements AutoCloseable {
+
+    @Override
+    public void close() throws Exception {
+        throw new NullPointerException("OOOPs. It is very bad to have a NullPointerException !!");
+    }
+
+    public void readFromResource() {
+        throw new RuntimeException("I am sorry. I don't have data available right now due to network issue !!!");
+    }
+}
