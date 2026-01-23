cbf79791c659d9c746277e84b1b8e54e75c8966f
Fatcat0712 <maisonhai456@gmail.com>
2026-01-24 00:55:45 +0700

    SafeVararges Annotation

---
+++ src/eazybytes/SafeVarExample.java
@@ -0,0 +1,53 @@
+package eazybytes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SafeVarExample {
+
+    public static void main(String[] args) {
+        sum(1);
+        sum(1,2);
+        sum(1,2,3);
+
+        SafeVarExample obj = new SafeVarExample();
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        List<String> list3 = new ArrayList<>();
+        list1.add("Eazy");
+        list1.add("Bytes");
+        list1.add("Java 7");
+        obj.print(list1, list2, list3);
+
+        /*
+        * List<Integer> nums = new ArrayList<>();
+        * nums.add(2);
+        * nums.add(3);
+        * obj.print(messages, nums);
+        * */
+
+
+    }
+
+
+    @SafeVarargs
+    public final void print(List<String>... messages) {
+//        Object[] arr = messages;
+//        List<Integer> intList = new ArrayList<>();
+//        intList.add(1606);
+//        arr[0] = intList;)
+        String firstElement = messages[0].get(0);
+        System.out.println(firstElement);
+    }
+
+    private static void sum(int... nums) {
+        System.out.println(Arrays.toString(nums));
+        int sum = 0;
+        for (int num : nums) {
+            sum = sum + num;
+        }
+        System.out.println("The total sum for the given input is: " + sum);
+    }
+
+}
