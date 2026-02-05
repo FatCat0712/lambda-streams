9bac7da7c963b09c93da48a50ea6ef8030acddbd
Fatcat0712 <maisonhai456@gmail.com>
2026-02-05 14:38:42 +0700

    FlatMap method in streams API

--- src/eazybytes/java8/streams/StreamOperations.java
+++ src/eazybytes/java8/streams/StreamOperations.java
@@ -1,11 +1,12 @@
 package eazybytes.java8.streams;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class StreamOperations {
     public static void main(String[] args) {
-        mapInStreams();
+//        mapInStreams();
+        flatMapInStreams();
     }
 
     public static void mapInStreams() {
@@ -15,6 +16,29 @@ public class StreamOperations {
         departmentList.add("Sales");
         departmentList.add("Marketing");
         departmentList.stream().map(word -> word.toUpperCase()).forEach(System.out::println);
+    }
+
+    public static void flatMapInStreams() {
+        String[] arrayOfWords = {"Eazy", "Bytes"};
+        Stream<String> streamOfWords = Arrays.stream(arrayOfWords);
+
+        /*streamOfWords
+                .map(word -> word.split(""))
+                .map(arr -> Arrays.stream(arr))
+                .forEach(System.out::println);*/
+
+        Stream<String[]> streamOflLetters = streamOfWords.map(word -> word.split(""));
+        streamOflLetters.flatMap(Arrays::stream).forEach(System.out::println);
+
+        List<List<String>> list = Arrays.asList(Arrays.asList("Eazy"), Arrays.asList("Bytes"));
+        System.out.println(list);
+        list.stream().map(Collection::stream).forEach(System.out::println);
+        list.stream().flatMap(Collection::stream).forEach(System.out::println);
+
+
+
+
+
     }
 
 
